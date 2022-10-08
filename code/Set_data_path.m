b45367b07fb031391ce1b00e018d022585c2a916
Hadis Jameei <hadisjameei@gmail.com>
2022-10-08 21:15:00 +1100

    Generate the subject IDs with self/ICD diagnostics

--- code/Set_data_path.m
+++ code/Set_data_path.m
@@ -4,7 +4,6 @@ path_old_dx_Maria = '/Users/mq669/Dropbox (Partners HealthCare)/DOCUMENTS/POSTDO
 Out_open_Maria='/Users/mq669/Dropbox (Partners HealthCare)/DOCUMENTS/POSTDOC_MNC/NHMRC Investigator grant/DATA MANAGEMENT/HETEROGENEITY2/Heterogeneity/outputs/Diagnosis/';
 Out_private_Maria='/Users/mq669/Dropbox (Partners HealthCare)/DOCUMENTS/POSTDOC_MNC/NHMRC Investigator grant/DATA MANAGEMENT/HETEROGENEITY2/Private_data/Private_data_out/';
 
-
 In_private_Spartan = '/home/dibiasem/punim0042/UKBIOBANK/diagnosis_in/';
 In_open_Spartan = '/home/dibiasem/punim0042/UKBIOBANK/diagnosis_in/';
 Out_open_Spartan = '/home/dibiasem/punim0042/UKBIOBANK/diagnosis_outNew/';
@@ -14,6 +13,7 @@ In_private_Hadis = '/Users/hadisjameei/Library/CloudStorage/OneDrive-TheUniversi
 In_open_Hadis = '/Users/hadisjameei/Library/CloudStorage/OneDrive-TheUniversityofMelbourne/PhD research/UK Biobank/Heterogeneity/Inputs/Diagnosis/';
 path_old_dx_Hadis = '/Users/hadisjameei/Library/CloudStorage/OneDrive-TheUniversityofMelbourne/PhD research/UK Biobank/Diagnoses/';
 Out_open_Hadis = '/Users/hadisjameei/Library/CloudStorage/OneDrive-TheUniversityofMelbourne/PhD research/UK Biobank/Heterogeneity/outputs/Diagnosis/';
+Out_private_Hadis = '/Users/hadisjameei/Library/CloudStorage/OneDrive-TheUniversityofMelbourne/PhD research/UK Biobank/Medical data/';
 
 In_private_Other = '';
 In_open_Other = '';
